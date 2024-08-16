34c218f8176e3d6a6a1173550d5f4bbbd97ac5fc
Leozinho2831 <leoferraz1818@gmail.com>
2024-08-16 20:35:22 -0300

    commit: scroll smooth class scroll smooth

--- Assets/Javascript/scrpit.js
+++ Assets/Javascript/scrpit.js
@@ -1,5 +1,26 @@
-function scrollSmooth(){
-    const linksHref = document.querySelector('.js-links a[href^="#"]');
-    console.log(linksHref);
+function initScroll(){
+    const linksHref = document.querySelectorAll('.js-links a[href^="#"]');
+    const sections = document.querySelectorAll('.js-sections');
+    const windowHight = window.innerHeight * 0.6;
+
+    if(linksHref && sections){
+
+        function scrollSmooth(event){
+            event.preventDefault();
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top - windowHight;
+    
+                section.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            });
+        }
+    
+        linksHref.forEach((linkHref) => {
+            linkHref.addEventListener('click', scrollSmooth);
+        });
+        
+    }
 }
-scrollSmooth();
+initScroll();
