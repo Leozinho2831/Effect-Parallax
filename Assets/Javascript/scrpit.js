3f9981817b5cc9a29737ba9e3726fd0b32c177a1
Leozinho2831 <leoferraz1818@gmail.com>
2024-08-16 21:01:48 -0300

    commit: remove const unused

--- Assets/Javascript/scrpit.js
+++ Assets/Javascript/scrpit.js
@@ -1,19 +1,18 @@
 function initScroll(){
     const linksHref = document.querySelectorAll('.js-links a[href^="#"]');
     const sections = document.querySelectorAll('.js-sections');
-    const windowHight = window.innerHeight * 0.6;
 
     if(linksHref && sections){
 
         function scrollSmooth(event){
             event.preventDefault();
             sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top - windowHight;
-    
+
                 section.scrollIntoView({
                     behavior: 'smooth',
                     block: 'start'
                 });
+
             });
         }
     
