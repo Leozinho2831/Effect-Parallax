1c1b9e9ace07a3ceae3e5412b000f24f4a0678dd
Leozinho2831 <leoferraz1818@gmail.com>
2024-08-17 13:44:30 -0300

    commit: new effect parallax

--- Assets/Javascript/scrpit.js
+++ Assets/Javascript/scrpit.js
@@ -21,3 +21,28 @@ function initScroll(){
     }
 }
 initScroll();
+
+function initParallax(){
+    const title = document.querySelector('.js-images h1');
+    const mount2 = document.querySelector('.js-images img:nth-of-type(1)');
+    const mount1 = document.querySelector('.js-images img:nth-of-type(2)');
+    const bush2 = document.querySelector('.js-images img:nth-of-type(3)');
+    const leaf2 = document.querySelector('.js-images img:nth-of-type(5)');
+    const leaf1 = document.querySelector('.js-images img:nth-of-type(6)');
+
+    if(title && mount2 && mount1 && bush2 && leaf2 && leaf1){
+        function parallax(){
+            const valueMargin = window.scrollY;
+
+            title.style.marginTop = `${valueMargin * 1.1}px`;
+            leaf1.style.marginLeft = `${-valueMargin}px`;
+            leaf2.style.marginLeft = `${valueMargin}px`;
+            bush2.style.marginBottom = `${-valueMargin}px`;
+            mount1.style.marginBottom = `${-valueMargin * 1.1}px`;
+            mount2.style.marginBottom = `${-valueMargin * 1.2}px`;
+        }
+
+        window.addEventListener('scroll', parallax);
+    }
+}
+initParallax()
